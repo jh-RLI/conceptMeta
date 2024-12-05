e35565dfc6e95346ce9b3f423057837490f57f51
jh-RLI <jonas.huber@rl-institut.de>
2024-12-05 11:53:57 +0100

    initial content

---
+++ README.md
@@ -0,0 +1,11 @@
+# ConceptMeta
+
+A CLI tool to manage metadata for data distributions using the INSIPRE metadata profile.
+
+## Usage
+
+-- metaPath
+
+--
+
+## Technology
---
+++ src/registerDistributions.py
@@ -0,0 +1,9 @@
+"""
+Register all dataset distributions that are part of the current run.
+This module helps to manage the available data and related metadata.
+Distributions relate to a file resource that is part of the ETL process.
+It must be determined if metadata is available or not.
+
+Note: To determine the metadata availability an automated approach should be
+used if possible
+"""
