dd608e8b3a93d18db8b629bdb716c6e789788c7f
jh-RLI <jonas.huber@rl-institut.de>
2024-12-16 16:35:41 +0100

    rework settings & add output path to store generated inspire documents

--- src/settings.py
+++ src/settings.py
@@ -10,13 +10,10 @@ BASE_PATH = Path.cwd()
 MODLUE_PATH = Path(BASE_PATH / "src")
 INPUT_CONFIG = Path(MODLUE_PATH / "config" / "config.yml")
 
+OUTPUT_PATH = Path(BASE_PATH / "output")
+
+
 # Load configuration
 config = load_config(INPUT_CONFIG)  # Replace with your YAML file path
-
-# Combine metadata_storage_path and metadata_document_name
-METADATA_FILE_PATH_STRING = (
-    f"{config['etl_processes']['metadata_storage_path']}"
-    f"{config['etl_processes']['metadata_document_name']}.xml"
-)
-
-META_FILE_PATH = Path(BASE_PATH / METADATA_FILE_PATH_STRING)
+# Access the ETL processes
+ETL_PROCESSES: list = config.get("etl_processes", [])
