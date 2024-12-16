6a18036688ea0622c100bc34896cfc31bb9ae145
jh-RLI <jonas.huber@rl-institut.de>
2024-12-16 13:43:17 +0100

    add xml module to parse inspire metadata using specific name spaces

---
+++ src/xml/namespaces.py
@@ -0,0 +1,9 @@
+namespaces = {
+    "gmd": "http://www.isotc211.org/2005/gmd",
+    "gco": "http://www.isotc211.org/2005/gco",
+    "gmx": "http://www.isotc211.org/2005/gmx",
+    "gml": "http://www.opengis.net/gml/3.2",
+    "xlink": "http://www.w3.org/1999/xlink",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "srv": "http://www.isotc211.org/2005/srv",
+}
---
+++ src/xml/utils.py
@@ -0,0 +1,2 @@
+# Collect based on one of the options below (depends on how this can be done in nodeRed)
+#
