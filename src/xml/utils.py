9bc5ff200aaa9b80cfe63c2a88feb26dfffe77d8
jh-RLI <jonas.huber@rl-institut.de>
2024-12-16 16:57:00 +0100

    rework xml save function

--- src/xml/utils.py
+++ src/xml/utils.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 from lxml import etree
 
+from settings import OUTPUT_PATH
 
-def save_metadata_document(metadata_root: etree.Element, file_path: Path):
+
+def save_metadata_document(metadata_root: etree.Element, file_name: str):
     """
     Saves the metadata document to a file.
 
@@ -13,11 +15,14 @@ def save_metadata_document(metadata_root: etree.Element, file_path: Path):
         metadata_root (etree.Element): The root element of the metadata XML document.
         file_path (Path): Path where the metadata document should be saved.
     """
+
+    xml_file_name = f"{file_name}.xml"
+    path = Path(OUTPUT_PATH / xml_file_name)
     # Ensure the parent directory exists
-    file_path.parent.mkdir(parents=True, exist_ok=True)
+    path.parent.mkdir(parents=True, exist_ok=True)
 
     # Write the XML to a file with pretty printing
-    with open(file_path, "wb") as file:
+    with open(path, "wb") as file:
         file.write(
             etree.tostring(
                 metadata_root, pretty_print=True, xml_declaration=True, encoding="UTF-8"
